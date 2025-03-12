3177cb6f7b3993bb4c9e1bf72145f0c5806f06e8
Mohammad Saifi <139645270+saifimd1234@users.noreply.github.com>
2025-03-12 17:43:10 +0530

    Update init_databse.sql syntax.

--- scripts/init_databse.sql
+++ scripts/init_databse.sql
@@ -1,5 +1,6 @@
+/*
 =============================================================
-/* Create Database and Schemas
+Create Database and Schemas
 =============================================================
 Script Purpose:
     This script creates a new database named 'DataWarehouse' after checking if it already exists. 
