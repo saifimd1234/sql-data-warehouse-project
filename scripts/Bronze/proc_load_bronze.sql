f7ecfa21b4fcaf905c85571467abc770a76817b7
saifimd1234 <139645270+saifimd1234@users.noreply.github.com>
2025-03-13 17:44:32 +0530

    Bulk insert the bronze.crm_cust_info from the csv file.

---
+++ scripts/Bronze/proc_load_bronze.sql
@@ -0,0 +1,7 @@
+BULK INSERT bronze.crm_cust_info
+FROM 'C:\Users\saifi\GitHub\sql-data-warehouse-project\datasets\source_crm\cust_info.csv'
+WITH (
+    FIRSTROW = 2,
+    FIELDTERMINATOR = ',',
+    TABLOCK
+);
