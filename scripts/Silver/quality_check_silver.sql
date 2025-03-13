363fb5f8a828a5b309df998b8a58613bf356b09a
saifimd1234 <139645270+saifimd1234@users.noreply.github.com>
2025-03-14 00:10:54 +0530

    Look at all the duplicate values in bronze layer (customer table).

--- scripts/Silver/quality_check_silver.sql
+++ scripts/Silver/quality_check_silver.sql
@@ -8,4 +8,13 @@ FROM bronze.crm_cust_info
 GROUP BY cst_id
 HAVING COUNT(*) > 1 OR cst_id IS NULL;
 
--- Output: There are duplicate values but no NULL values.
+-- Output: There are duplicate values but no NULL values. Same cst_id is has multiple cst_create_date.
+
+-- Look at all the duplicate values.
+SELECT
+*
+FROM bronze.crm_cust_info
+WHERE cst_id IN (SELECT cst_id FROM bronze.crm_cust_info GROUP BY cst_id HAVING COUNT(*) > 1);
+
+-- We can resolve it by keeping the latest record and deleting the rest.
+
