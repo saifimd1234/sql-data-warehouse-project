636120e4fcf4cd7ae1ff549624d4e5d3cf29567a
saifimd1234 <139645270+saifimd1234@users.noreply.github.com>
2025-03-13 22:03:59 +0530

    Basic script to load the bronze layer.

--- scripts/Bronze/proc_load_bronze.sql
+++ scripts/Bronze/proc_load_bronze.sql
@@ -18,7 +18,8 @@ WITH (
     TABLOCK
 );
 
-TRUNCATE TABLE bronze.crm_sales_details
+TRUNCATE TABLE bronze.crm_sales_details;
+BULK INSERT bronze.crm_sales_details
 FROM 'C:\Users\saifi\GitHub\sql-data-warehouse-project\datasets\source_crm\sales_details.csv'
 WITH (
     FIRSTROW = 2,
@@ -26,7 +27,8 @@ WITH (
     TABLOCK
 );
 
-TRUNCATE TABLE bronze.erp_loc_a101
+TRUNCATE TABLE bronze.erp_loc_a101;
+BULK INSERT bronze.erp_loc_a101
 FROM 'C:\Users\saifi\GitHub\sql-data-warehouse-project\datasets\source_erp\LOC_A101.csv'
 WITH (
     FIRSTROW = 2,
@@ -35,6 +37,7 @@ WITH (
 );
 
 TRUNCATE TABLE bronze.erp_cust_az12
+BULK INSERT bronze.erp_cust_az12
 FROM 'C:\Users\saifi\GitHub\sql-data-warehouse-project\datasets\source_erp\CUST_AZ12.csv'
 WITH (
     FIRSTROW = 2,
@@ -43,6 +46,7 @@ WITH (
 );
 
 TRUNCATE TABLE bronze.erp_px_cat_g1v2
+BULK INSERT bronze.erp_px_cat_g1v2
 FROM 'C:\Users\saifi\GitHub\sql-data-warehouse-project\datasets\source_erp\PX_CAT_G1V2.csv'
 WITH (
     FIRSTROW = 2,
