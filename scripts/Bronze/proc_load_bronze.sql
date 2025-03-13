d464bd47742c021fafef15999d71fbeac3621af6
saifimd1234 <139645270+saifimd1234@users.noreply.github.com>
2025-03-13 22:23:39 +0530

    Comment for truncating and inserting.

--- scripts/Bronze/proc_load_bronze.sql
+++ scripts/Bronze/proc_load_bronze.sql
@@ -9,8 +9,9 @@ BEGIN
 	PRINT 'Loading CRM Tables';
 	PRINT '------------------------------------------------';
 
+	PRINT '>> Truncating Table: bronze.crm_cust_info';
 	TRUNCATE TABLE bronze.crm_cust_info;
-
+	PRINT '>> Inserting Data Into: bronze.crm_cust_info';
 	BULK INSERT bronze.crm_cust_info
 	FROM 'C:\Users\saifi\GitHub\sql-data-warehouse-project\datasets\source_crm\cust_info.csv'
 	WITH (
@@ -19,8 +20,9 @@ BEGIN
 		TABLOCK
 	);
 
+	PRINT '>> Truncating Table: bronze.crm_prd_info';
 	TRUNCATE TABLE bronze.crm_prd_info;
-
+	PRINT '>> Inserting Data Into: bronze.crm_prd_info';
 	BULK INSERT bronze.crm_prd_info
 	FROM 'C:\Users\saifi\GitHub\sql-data-warehouse-project\datasets\source_crm\prd_info.csv'
 	WITH (
@@ -29,7 +31,9 @@ BEGIN
 		TABLOCK
 	);
 
+	PRINT '>> Truncating Table: bronze.crm_sales_details';
 	TRUNCATE TABLE bronze.crm_sales_details;
+	PRINT '>> Inserting Data Into: bronze.crm_sales_details';
 	BULK INSERT bronze.crm_sales_details
 	FROM 'C:\Users\saifi\GitHub\sql-data-warehouse-project\datasets\source_crm\sales_details.csv'
 	WITH (
@@ -42,7 +46,9 @@ BEGIN
 	PRINT 'Loading ERP Tables';
 	PRINT '------------------------------------------------';
 
+	PRINT '>> Truncating Table: bronze.erp_loc_a101';
 	TRUNCATE TABLE bronze.erp_loc_a101;
+	PRINT '>> Inserting Data Into: bronze.erp_loc_a101';
 	BULK INSERT bronze.erp_loc_a101
 	FROM 'C:\Users\saifi\GitHub\sql-data-warehouse-project\datasets\source_erp\LOC_A101.csv'
 	WITH (
@@ -51,7 +57,9 @@ BEGIN
 		TABLOCK
 	);
 
+	PRINT '>> Truncating Table: bronze.erp_cust_az12';
 	TRUNCATE TABLE bronze.erp_cust_az12
+	PRINT '>> Inserting Data Into: bronze.erp_cust_az12';
 	BULK INSERT bronze.erp_cust_az12
 	FROM 'C:\Users\saifi\GitHub\sql-data-warehouse-project\datasets\source_erp\CUST_AZ12.csv'
 	WITH (
@@ -60,7 +68,9 @@ BEGIN
 		TABLOCK
 	);
 
+	PRINT '>> Truncating Table: bronze.erp_px_cat_g1v2';
 	TRUNCATE TABLE bronze.erp_px_cat_g1v2
+	PRINT '>> Inserting Data Into: bronze.erp_px_cat_g1v2';
 	BULK INSERT bronze.erp_px_cat_g1v2
 	FROM 'C:\Users\saifi\GitHub\sql-data-warehouse-project\datasets\source_erp\PX_CAT_G1V2.csv'
 	WITH (
