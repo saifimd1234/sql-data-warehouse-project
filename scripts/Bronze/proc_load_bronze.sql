360f808b116aa19adfd043471f1a0d7c848c196b
saifimd1234 <139645270+saifimd1234@users.noreply.github.com>
2025-03-13 22:27:07 +0530

    Create a try/catch block for future debug.

--- scripts/Bronze/proc_load_bronze.sql
+++ scripts/Bronze/proc_load_bronze.sql
@@ -1,5 +1,7 @@
 CREATE OR ALTER PROCEDURE bronze.load_bronze AS
 BEGIN
+BEGIN TRY
+
 	PRINT '================================================';
 	PRINT 'Loading Bronze Layer';
 	PRINT '================================================';
@@ -78,4 +80,13 @@ BEGIN
 		FIELDTERMINATOR = ',',
 		TABLOCK
 	);
+END TRY
+BEGIN CATCH
+	PRINT '==========================================';
+	PRINT 'ERROR OCCURED DURING LOADING BRONZE LAYER';
+	PRINT 'Error Message' + ERROR_MESSAGE();
+	PRINT 'Error Message' + CAST (ERROR_NUMBER() AS NVARCHAR);
+	PRINT 'Error Message' + CAST (ERROR_STATE() AS NVARCHAR);
+	PRINT '==========================================';
+END CATCH
 END
