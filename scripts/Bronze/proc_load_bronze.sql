6af432ca6e448050b05b395c22ace63e14e5ce1e
saifimd1234 <139645270+saifimd1234@users.noreply.github.com>
2025-03-13 22:18:25 +0530

    Correct the indentation.

--- scripts/Bronze/proc_load_bronze.sql
+++ scripts/Bronze/proc_load_bronze.sql
@@ -1,59 +1,59 @@
 CREATE OR ALTER PROCEDURE bronze.load_bronze AS
 BEGIN
 
-TRUNCATE TABLE bronze.crm_cust_info;
+	TRUNCATE TABLE bronze.crm_cust_info;
 
-BULK INSERT bronze.crm_cust_info
-FROM 'C:\Users\saifi\GitHub\sql-data-warehouse-project\datasets\source_crm\cust_info.csv'
-WITH (
-    FIRSTROW = 2,
-    FIELDTERMINATOR = ',',
-    TABLOCK
-);
+	BULK INSERT bronze.crm_cust_info
+	FROM 'C:\Users\saifi\GitHub\sql-data-warehouse-project\datasets\source_crm\cust_info.csv'
+	WITH (
+		FIRSTROW = 2,
+		FIELDTERMINATOR = ',',
+		TABLOCK
+	);
 
-TRUNCATE TABLE bronze.crm_prd_info;
+	TRUNCATE TABLE bronze.crm_prd_info;
 
-BULK INSERT bronze.crm_prd_info
-FROM 'C:\Users\saifi\GitHub\sql-data-warehouse-project\datasets\source_crm\prd_info.csv'
-WITH (
-    FIRSTROW = 2,
-    FIELDTERMINATOR = ',',
-    TABLOCK
-);
+	BULK INSERT bronze.crm_prd_info
+	FROM 'C:\Users\saifi\GitHub\sql-data-warehouse-project\datasets\source_crm\prd_info.csv'
+	WITH (
+		FIRSTROW = 2,
+		FIELDTERMINATOR = ',',
+		TABLOCK
+	);
 
-TRUNCATE TABLE bronze.crm_sales_details;
-BULK INSERT bronze.crm_sales_details
-FROM 'C:\Users\saifi\GitHub\sql-data-warehouse-project\datasets\source_crm\sales_details.csv'
-WITH (
-    FIRSTROW = 2,
-    FIELDTERMINATOR = ',',
-    TABLOCK
-);
+	TRUNCATE TABLE bronze.crm_sales_details;
+	BULK INSERT bronze.crm_sales_details
+	FROM 'C:\Users\saifi\GitHub\sql-data-warehouse-project\datasets\source_crm\sales_details.csv'
+	WITH (
+		FIRSTROW = 2,
+		FIELDTERMINATOR = ',',
+		TABLOCK
+	);
 
-TRUNCATE TABLE bronze.erp_loc_a101;
-BULK INSERT bronze.erp_loc_a101
-FROM 'C:\Users\saifi\GitHub\sql-data-warehouse-project\datasets\source_erp\LOC_A101.csv'
-WITH (
-    FIRSTROW = 2,
-    FIELDTERMINATOR = ',',
-    TABLOCK
-);
+	TRUNCATE TABLE bronze.erp_loc_a101;
+	BULK INSERT bronze.erp_loc_a101
+	FROM 'C:\Users\saifi\GitHub\sql-data-warehouse-project\datasets\source_erp\LOC_A101.csv'
+	WITH (
+		FIRSTROW = 2,
+		FIELDTERMINATOR = ',',
+		TABLOCK
+	);
 
-TRUNCATE TABLE bronze.erp_cust_az12
-BULK INSERT bronze.erp_cust_az12
-FROM 'C:\Users\saifi\GitHub\sql-data-warehouse-project\datasets\source_erp\CUST_AZ12.csv'
-WITH (
-    FIRSTROW = 2,
-    FIELDTERMINATOR = ',',
-    TABLOCK
-);
+	TRUNCATE TABLE bronze.erp_cust_az12
+	BULK INSERT bronze.erp_cust_az12
+	FROM 'C:\Users\saifi\GitHub\sql-data-warehouse-project\datasets\source_erp\CUST_AZ12.csv'
+	WITH (
+		FIRSTROW = 2,
+		FIELDTERMINATOR = ',',
+		TABLOCK
+	);
 
-TRUNCATE TABLE bronze.erp_px_cat_g1v2
-BULK INSERT bronze.erp_px_cat_g1v2
-FROM 'C:\Users\saifi\GitHub\sql-data-warehouse-project\datasets\source_erp\PX_CAT_G1V2.csv'
-WITH (
-    FIRSTROW = 2,
-    FIELDTERMINATOR = ',',
-    TABLOCK
-);
+	TRUNCATE TABLE bronze.erp_px_cat_g1v2
+	BULK INSERT bronze.erp_px_cat_g1v2
+	FROM 'C:\Users\saifi\GitHub\sql-data-warehouse-project\datasets\source_erp\PX_CAT_G1V2.csv'
+	WITH (
+		FIRSTROW = 2,
+		FIELDTERMINATOR = ',',
+		TABLOCK
+	);
 END
