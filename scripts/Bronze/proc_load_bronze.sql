443520f931345e828142bb14193ae1b9ddf063c0
saifimd1234 <139645270+saifimd1234@users.noreply.github.com>
2025-03-13 22:28:01 +0530

    Correct indentation.

--- scripts/Bronze/proc_load_bronze.sql
+++ scripts/Bronze/proc_load_bronze.sql
@@ -1,92 +1,92 @@
 CREATE OR ALTER PROCEDURE bronze.load_bronze AS
 BEGIN
-BEGIN TRY
+	BEGIN TRY
 
-	PRINT '================================================';
-	PRINT 'Loading Bronze Layer';
-	PRINT '================================================';
+		PRINT '================================================';
+		PRINT 'Loading Bronze Layer';
+		PRINT '================================================';
 	
 	
-	PRINT '------------------------------------------------';
-	PRINT 'Loading CRM Tables';
-	PRINT '------------------------------------------------';
+		PRINT '------------------------------------------------';
+		PRINT 'Loading CRM Tables';
+		PRINT '------------------------------------------------';
 
-	PRINT '>> Truncating Table: bronze.crm_cust_info';
-	TRUNCATE TABLE bronze.crm_cust_info;
-	PRINT '>> Inserting Data Into: bronze.crm_cust_info';
-	BULK INSERT bronze.crm_cust_info
-	FROM 'C:\Users\saifi\GitHub\sql-data-warehouse-project\datasets\source_crm\cust_info.csv'
-	WITH (
-		FIRSTROW = 2,
-		FIELDTERMINATOR = ',',
-		TABLOCK
-	);
+		PRINT '>> Truncating Table: bronze.crm_cust_info';
+		TRUNCATE TABLE bronze.crm_cust_info;
+		PRINT '>> Inserting Data Into: bronze.crm_cust_info';
+		BULK INSERT bronze.crm_cust_info
+		FROM 'C:\Users\saifi\GitHub\sql-data-warehouse-project\datasets\source_crm\cust_info.csv'
+		WITH (
+			FIRSTROW = 2,
+			FIELDTERMINATOR = ',',
+			TABLOCK
+		);
 
-	PRINT '>> Truncating Table: bronze.crm_prd_info';
-	TRUNCATE TABLE bronze.crm_prd_info;
-	PRINT '>> Inserting Data Into: bronze.crm_prd_info';
-	BULK INSERT bronze.crm_prd_info
-	FROM 'C:\Users\saifi\GitHub\sql-data-warehouse-project\datasets\source_crm\prd_info.csv'
-	WITH (
-		FIRSTROW = 2,
-		FIELDTERMINATOR = ',',
-		TABLOCK
-	);
+		PRINT '>> Truncating Table: bronze.crm_prd_info';
+		TRUNCATE TABLE bronze.crm_prd_info;
+		PRINT '>> Inserting Data Into: bronze.crm_prd_info';
+		BULK INSERT bronze.crm_prd_info
+		FROM 'C:\Users\saifi\GitHub\sql-data-warehouse-project\datasets\source_crm\prd_info.csv'
+		WITH (
+			FIRSTROW = 2,
+			FIELDTERMINATOR = ',',
+			TABLOCK
+		);
 
-	PRINT '>> Truncating Table: bronze.crm_sales_details';
-	TRUNCATE TABLE bronze.crm_sales_details;
-	PRINT '>> Inserting Data Into: bronze.crm_sales_details';
-	BULK INSERT bronze.crm_sales_details
-	FROM 'C:\Users\saifi\GitHub\sql-data-warehouse-project\datasets\source_crm\sales_details.csv'
-	WITH (
-		FIRSTROW = 2,
-		FIELDTERMINATOR = ',',
-		TABLOCK
-	);
+		PRINT '>> Truncating Table: bronze.crm_sales_details';
+		TRUNCATE TABLE bronze.crm_sales_details;
+		PRINT '>> Inserting Data Into: bronze.crm_sales_details';
+		BULK INSERT bronze.crm_sales_details
+		FROM 'C:\Users\saifi\GitHub\sql-data-warehouse-project\datasets\source_crm\sales_details.csv'
+		WITH (
+			FIRSTROW = 2,
+			FIELDTERMINATOR = ',',
+			TABLOCK
+		);
 
-	PRINT '------------------------------------------------';
-	PRINT 'Loading ERP Tables';
-	PRINT '------------------------------------------------';
+		PRINT '------------------------------------------------';
+		PRINT 'Loading ERP Tables';
+		PRINT '------------------------------------------------';
 
-	PRINT '>> Truncating Table: bronze.erp_loc_a101';
-	TRUNCATE TABLE bronze.erp_loc_a101;
-	PRINT '>> Inserting Data Into: bronze.erp_loc_a101';
-	BULK INSERT bronze.erp_loc_a101
-	FROM 'C:\Users\saifi\GitHub\sql-data-warehouse-project\datasets\source_erp\LOC_A101.csv'
-	WITH (
-		FIRSTROW = 2,
-		FIELDTERMINATOR = ',',
-		TABLOCK
-	);
+		PRINT '>> Truncating Table: bronze.erp_loc_a101';
+		TRUNCATE TABLE bronze.erp_loc_a101;
+		PRINT '>> Inserting Data Into: bronze.erp_loc_a101';
+		BULK INSERT bronze.erp_loc_a101
+		FROM 'C:\Users\saifi\GitHub\sql-data-warehouse-project\datasets\source_erp\LOC_A101.csv'
+		WITH (
+			FIRSTROW = 2,
+			FIELDTERMINATOR = ',',
+			TABLOCK
+		);
 
-	PRINT '>> Truncating Table: bronze.erp_cust_az12';
-	TRUNCATE TABLE bronze.erp_cust_az12
-	PRINT '>> Inserting Data Into: bronze.erp_cust_az12';
-	BULK INSERT bronze.erp_cust_az12
-	FROM 'C:\Users\saifi\GitHub\sql-data-warehouse-project\datasets\source_erp\CUST_AZ12.csv'
-	WITH (
-		FIRSTROW = 2,
-		FIELDTERMINATOR = ',',
-		TABLOCK
-	);
+		PRINT '>> Truncating Table: bronze.erp_cust_az12';
+		TRUNCATE TABLE bronze.erp_cust_az12
+		PRINT '>> Inserting Data Into: bronze.erp_cust_az12';
+		BULK INSERT bronze.erp_cust_az12
+		FROM 'C:\Users\saifi\GitHub\sql-data-warehouse-project\datasets\source_erp\CUST_AZ12.csv'
+		WITH (
+			FIRSTROW = 2,
+			FIELDTERMINATOR = ',',
+			TABLOCK
+		);
 
-	PRINT '>> Truncating Table: bronze.erp_px_cat_g1v2';
-	TRUNCATE TABLE bronze.erp_px_cat_g1v2
-	PRINT '>> Inserting Data Into: bronze.erp_px_cat_g1v2';
-	BULK INSERT bronze.erp_px_cat_g1v2
-	FROM 'C:\Users\saifi\GitHub\sql-data-warehouse-project\datasets\source_erp\PX_CAT_G1V2.csv'
-	WITH (
-		FIRSTROW = 2,
-		FIELDTERMINATOR = ',',
-		TABLOCK
-	);
-END TRY
-BEGIN CATCH
-	PRINT '==========================================';
-	PRINT 'ERROR OCCURED DURING LOADING BRONZE LAYER';
-	PRINT 'Error Message' + ERROR_MESSAGE();
-	PRINT 'Error Message' + CAST (ERROR_NUMBER() AS NVARCHAR);
-	PRINT 'Error Message' + CAST (ERROR_STATE() AS NVARCHAR);
-	PRINT '==========================================';
-END CATCH
+		PRINT '>> Truncating Table: bronze.erp_px_cat_g1v2';
+		TRUNCATE TABLE bronze.erp_px_cat_g1v2
+		PRINT '>> Inserting Data Into: bronze.erp_px_cat_g1v2';
+		BULK INSERT bronze.erp_px_cat_g1v2
+		FROM 'C:\Users\saifi\GitHub\sql-data-warehouse-project\datasets\source_erp\PX_CAT_G1V2.csv'
+		WITH (
+			FIRSTROW = 2,
+			FIELDTERMINATOR = ',',
+			TABLOCK
+		);
+	END TRY
+	BEGIN CATCH
+		PRINT '==========================================';
+		PRINT 'ERROR OCCURED DURING LOADING BRONZE LAYER';
+		PRINT 'Error Message' + ERROR_MESSAGE();
+		PRINT 'Error Message' + CAST (ERROR_NUMBER() AS NVARCHAR);
+		PRINT 'Error Message' + CAST (ERROR_STATE() AS NVARCHAR);
+		PRINT '==========================================';
+	END CATCH
 END
