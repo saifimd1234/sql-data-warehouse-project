d91f39059d58d61fb523e8507f041913a25525ca
saifimd1234 <139645270+saifimd1234@users.noreply.github.com>
2025-03-14 00:51:09 +0530

    standardized data for consistency in queries.
    Apply data standardization on cst_marital_status and cst_gndr.

--- scripts/Silver/quality_check_silver.sql
+++ scripts/Silver/quality_check_silver.sql
@@ -49,13 +49,25 @@ SELECT cst_lastname
 FROM bronze.crm_cust_info
 WHERE cst_lastname != TRIM(cst_lastname);
 
+-- Data Standaraization & Consistency
+SELECT DISTINCT cst_gndr
+FROM bronze.crm_cust_info;
+
 SELECT
 	cst_id,
 	cst_key,
 	TRIM(cst_firstname) AS cst_firstname,
 	TRIM(cst_lastname) AS cst_lastname,
-	cst_marital_status,
-	cst_gndr,
+    CASE
+        WHEN UPPER(TRIM(cst_marital_status)) = 'F' THEN 'Female'
+        WHEN UPPER(TRIM(cst_marital_status)) = 'M' THEN 'Male'
+        ELSE 'n/a'
+    END AS cst_marital_status,
+	CASE
+        WHEN UPPER(TRIM(cst_gndr)) = 'F' THEN 'Female'
+        WHEN UPPER(TRIM(cst_gndr)) = 'M' THEN 'Male'
+        ELSE 'n/a'
+    END AS cst_gndr,
 	cst_create_date
 FROM (
 	SELECT
@@ -64,4 +76,5 @@ FROM (
     FROM bronze.crm_cust_info
 	WHERE cst_id IS NOT NULL
 )t WHERE flag_last = 1
+
 
