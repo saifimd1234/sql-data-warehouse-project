6c6faa6cdd65a59a6a90ce558fb56de91f419b9b
saifimd1234 <139645270+saifimd1234@users.noreply.github.com>
2025-03-13 17:20:49 +0530

    Create bronze.crm_prd_info table.

--- scripts/Bronze/ddl_bronze.sql
+++ scripts/Bronze/ddl_bronze.sql
@@ -8,4 +8,12 @@ CREATE TABLE bronze.crm_cust_info (
     cst_create_date     DATE
 );
 
-SELECT * from bronze.crm_cust_info;
+CREATE TABLE bronze.crm_prd_info (
+    prd_id       INT,
+    prd_key      NVARCHAR(50),
+    prd_nm       NVARCHAR(50),
+    prd_cost     INT,
+    prd_line     NVARCHAR(50),
+    prd_start_dt DATETIME,
+    prd_end_dt   DATETIME
+);
