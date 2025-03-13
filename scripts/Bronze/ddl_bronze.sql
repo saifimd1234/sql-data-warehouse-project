229a749f141c5892823875fb66af75a597138d83
saifimd1234 <139645270+saifimd1234@users.noreply.github.com>
2025-03-13 17:23:18 +0530

    Simiarly create 3 new tables for erp.

--- scripts/Bronze/ddl_bronze.sql
+++ scripts/Bronze/ddl_bronze.sql
@@ -29,3 +29,22 @@ CREATE TABLE bronze.crm_sales_details (
     sls_quantity INT,
     sls_price    INT
 );
+
+
+CREATE TABLE bronze.erp_loc_a101 (
+    cid    NVARCHAR(50),
+    cntry  NVARCHAR(50)
+);
+
+CREATE TABLE bronze.erp_cust_az12 (
+    cid    NVARCHAR(50),
+    bdate  DATE,
+    gen    NVARCHAR(50)
+);
+
+CREATE TABLE bronze.erp_px_cat_g1v2 (
+    id           NVARCHAR(50),
+    cat          NVARCHAR(50),
+    subcat       NVARCHAR(50),
+    maintenance  NVARCHAR(50)
+);
