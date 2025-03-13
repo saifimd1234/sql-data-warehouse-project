9504835c25716422abb04bc8311007806b7de3a4
saifimd1234 <139645270+saifimd1234@users.noreply.github.com>
2025-03-14 00:22:45 +0530

    Query to get the latest record.
    The last(latest) query is for the latest record and the above two indentation has been corrected.

--- scripts/Silver/quality_check_silver.sql
+++ scripts/Silver/quality_check_silver.sql
@@ -2,8 +2,8 @@
 -- Expectation: No Results
 
 SELECT 
-cst_id,
-COUNT(*)
+    cst_id,
+    COUNT(*)
 FROM bronze.crm_cust_info
 GROUP BY cst_id
 HAVING COUNT(*) > 1 OR cst_id IS NULL;
@@ -12,14 +12,29 @@ HAVING COUNT(*) > 1 OR cst_id IS NULL;
 
 -- Look at all the duplicate values.
 SELECT
-*
+    *
 FROM bronze.crm_cust_info
-WHERE cst_id IN (SELECT cst_id FROM bronze.crm_cust_info GROUP BY cst_id HAVING COUNT(*) > 1);
+WHERE cst_id IN (
+    SELECT cst_id 
+    FROM bronze.crm_cust_info 
+    GROUP BY cst_id 
+    HAVING COUNT(*) > 1
+);
 
 -- We can resolve it by keeping the latest record and deleting the rest.
 
 SELECT
-*,
-ROW_NUMBER() OVER(PARTITION BY cst_id ORDER BY cst_create_date DESC) AS flag_last
-FROM bronze.crm_cust_info
-WHERE cst_id IN (SELECT cst_id FROM bronze.crm_cust_info GROUP BY cst_id HAVING COUNT(*) > 1);
+    *
+FROM (
+    SELECT
+        *,
+        ROW_NUMBER() OVER(PARTITION BY cst_id ORDER BY cst_create_date DESC) AS flag_last
+    FROM bronze.crm_cust_info
+) t
+WHERE flag_last = 1 
+AND cst_id IN (
+    SELECT cst_id 
+    FROM bronze.crm_cust_info 
+    GROUP BY cst_id 
+    HAVING COUNT(*) > 1
+);
