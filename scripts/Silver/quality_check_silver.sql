2bee8eae770edd255fe9f9a44a6bcdf8712f036c
saifimd1234 <139645270+saifimd1234@users.noreply.github.com>
2025-03-14 00:31:29 +0530

    Clean firstname, lastname column of any unwanted spaces.

--- scripts/Silver/quality_check_silver.sql
+++ scripts/Silver/quality_check_silver.sql
@@ -49,3 +49,19 @@ SELECT cst_lastname
 FROM bronze.crm_cust_info
 WHERE cst_lastname != TRIM(cst_lastname);
 
+SELECT
+	cst_id,
+	cst_key,
+	TRIM(cst_firstname) AS cst_firstname,
+	TRIM(cst_lastname) AS cst_lastname,
+	cst_marital_status,
+	cst_gndr,
+	cst_create_date
+FROM (
+	SELECT
+        *,
+        ROW_NUMBER() OVER(PARTITION BY cst_id ORDER BY cst_create_date DESC) AS flag_last
+    FROM bronze.crm_cust_info
+	WHERE cst_id IS NOT NULL
+)t WHERE flag_last = 1
+
