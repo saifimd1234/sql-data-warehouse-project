a601fe66bd643c108425219616d4d6b15c4f0f07
saifimd1234 <139645270+saifimd1234@users.noreply.github.com>
2025-03-14 16:18:12 +0530

    Check Data Consistency: Between Sales, Quantity, and Price

--- scripts/Silver/quality_check_silver.sql
+++ scripts/Silver/quality_check_silver.sql
@@ -232,3 +232,15 @@ SELECT
 FROM bronze.crm_sales_details
 WHERE sls_order_dt > sls_ship_dt
 OR sls_order_dt > sls_due_dt;
+
+-- Check Data Consistency: Between Sales, Quantity, and Price
+-- >> Sales = Quantity * Price
+-- >> Vallues must not be NULL, zero or negative.
+SELECT
+    sls_sales,
+    sls_quantity,
+    sls_price
+FROM bronze.crm_sales_details
+WHERE sls_sales != sls_quantity * sls_price
+OR sls_sales IS NULL OR sls_quantity IS NULL OR sls_price IS NULL
+OR sls_sales <= 0 OR sls_quantity <= 0 OR sls_price <= 0;
