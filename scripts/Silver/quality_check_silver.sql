816a005990b9ce0c5a9cde7ce042becfbbf831e1
saifimd1234 <139645270+saifimd1234@users.noreply.github.com>
2025-03-14 16:01:56 +0530

    check the integrity of the customers columns in the silver layer
    Using NOT IN we can check the sls_cust_id that are not present in cst_id in other table.

--- scripts/Silver/quality_check_silver.sql
+++ scripts/Silver/quality_check_silver.sql
@@ -180,5 +180,25 @@ SELECT
 FROM [DataWarehouse].[bronze].[crm_sales_details]
 WHERE sls_prd_key NOT IN (
     SELECT prd_key
-    FROM [DataWarehouse].[bronze].[crm_prd_info]
+    FROM [DataWarehouse].[silver].[crm_prd_info]
+);
+
+-- Similarly, we can check the integrity of the customers columns in the silver layer.
+-- Using NOT IN we can check the sls_cust_id that are not present in cst_id in other table.
+-- Since, we are not getting any output that means everything is good and we can go and connect them without any issues.
+
+SELECT
+    [sls_ord_num],
+    [sls_prd_key],
+    [sls_cust_id],
+    [sls_order_dt],
+    [sls_ship_dt],
+    [sls_due_dt],
+    [sls_sales],
+    [sls_quantity],
+    [sls_price]
+FROM [DataWarehouse].[bronze].[crm_sales_details]
+WHERE sls_cust_id NOT IN (
+    SELECT cst_id
+    FROM [DataWarehouse].[silver].[crm_cust_info]
 );
