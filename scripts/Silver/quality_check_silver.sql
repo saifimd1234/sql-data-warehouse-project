35765d2b98606eb566880272a32d1fcb67d0e0a8
saifimd1234 <139645270+saifimd1234@users.noreply.github.com>
2025-03-14 15:46:50 +0530

    These quality checks are done before transformation.

--- scripts/Silver/quality_check_silver.sql
+++ scripts/Silver/quality_check_silver.sql
@@ -110,3 +110,31 @@ SELECT DISTINCT cst_gndr
 FROM silver.crm_cust_info;
 
 -- =============================================================
+
+-- QUALITY CHECK FOR BRONZE LAYER. (Table: bronze.crm_prd_info)
+
+-- Check for NULLs or Duplicates in Primary Key.
+-- Expectation: No Results
+SELECT
+    prd_id,
+    COUNT(*)
+FROM bronze.crm_prd_info
+GROUP BY prd_id
+HAVING COUNT(*) > 1 OR prd_id IS NULL;
+
+-- Check for unwanted spaces.
+-- Expectation: No Results
+SELECT prd_nm
+FROM bronze.crm_prd_info
+WHERE prd_nm != TRIM(prd_nm);
+
+-- Check for NULLs or Negative Numbers
+-- Expectation: No Results
+SELECT prd_cost
+FROM bronze.crm_prd_info
+WHERE prd_cost < 0 OR prd_cost IS NULL;
+
+-- Data Standardization & Consistency
+-- Expectation: No Results
+SELECT DISTINCT prd_line
+FROM bronze.crm_prd_info;
