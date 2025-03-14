5eee6ca4668273ca922aa557e6ce93459ecfb544
saifimd1234 <139645270+saifimd1234@users.noreply.github.com>
2025-03-14 19:25:09 +0530

    Update the DDL for silver.erp_loc_a101

--- scripts/Silver/ddl_silver.sql
+++ scripts/Silver/ddl_silver.sql
@@ -60,13 +60,14 @@ CREATE TABLE silver.crm_sales_details (
 );
 GO
 
-IF OBJECT_ID('bronze.erp_loc_a101', 'U') IS NOT NULL
-    DROP TABLE bronze.erp_loc_a101;
+IF OBJECT_ID('silver.erp_loc_a101', 'U') IS NOT NULL
+    DROP TABLE silver.erp_loc_a101;
 GO
 
-CREATE TABLE bronze.erp_loc_a101 (
+CREATE TABLE silver.erp_loc_a101 (
     cid    NVARCHAR(50),
-    cntry  NVARCHAR(50)
+    cntry  NVARCHAR(50),
+	dwh_create_date DATETIME2 DEFAULT GETDATE()
 );
 GO
 
