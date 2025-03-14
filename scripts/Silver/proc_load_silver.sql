bac20e6fbe5caf8313db7522923c81695ba9b291
saifimd1234 <139645270+saifimd1234@users.noreply.github.com>
2025-03-14 16:15:11 +0530

    Date transformation for silver.crm_sales_details

--- scripts/Silver/proc_load_silver.sql
+++ scripts/Silver/proc_load_silver.sql
@@ -65,9 +65,18 @@ SELECT
     [sls_ord_num],
     [sls_prd_key],
     [sls_cust_id],
-    [sls_order_dt],
-    [sls_ship_dt],
-    [sls_due_dt],
+    CASE 
+        WHEN sls_order_dt = 0 OR LEN(sls_order_dt) != 8 THEN NULL
+        ELSE CAST(CAST(sls_order_dt AS VARCHAR) AS DATE)
+    END AS [sls_order_dt],
+     CASE 
+        WHEN sls_ship_dt = 0 OR LEN(sls_ship_dt) != 8 THEN NULL
+        ELSE CAST(CAST(sls_ship_dt AS VARCHAR) AS DATE)
+    END AS [sls_ship_dt],
+     CASE 
+        WHEN sls_due_dt = 0 OR LEN(sls_due_dt) != 8 THEN NULL
+        ELSE CAST(CAST(sls_due_dt AS VARCHAR) AS DATE)
+    END AS [sls_due_dt],
     [sls_sales],
     [sls_quantity],
     [sls_price]
