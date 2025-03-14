51b04693713a47d6b06d45d6f962a2393fec4254
saifimd1234 <139645270+saifimd1234@users.noreply.github.com>
2025-03-14 19:36:45 +0530

    Update DDL for silver.erp_px_cat_g1v2

--- scripts/Silver/ddl_silver.sql
+++ scripts/Silver/ddl_silver.sql
@@ -83,15 +83,16 @@ CREATE TABLE silver.erp_cust_az12 (
 );
 GO
 
-IF OBJECT_ID('bronze.erp_px_cat_g1v2', 'U') IS NOT NULL
-    DROP TABLE bronze.erp_px_cat_g1v2;
+IF OBJECT_ID('silver.erp_px_cat_g1v2', 'U') IS NOT NULL
+    DROP TABLE silver.erp_px_cat_g1v2;
 GO
 
-CREATE TABLE bronze.erp_px_cat_g1v2 (
+CREATE TABLE silver.erp_px_cat_g1v2 (
     id           NVARCHAR(50),
     cat          NVARCHAR(50),
     subcat       NVARCHAR(50),
-    maintenance  NVARCHAR(50)
+    maintenance  NVARCHAR(50),
+	dwh_create_date DATETIME2 DEFAULT GETDATE()
 );
 GO
 
