3e8f55478077569b58ce35caf488c314acec2faf
saifimd1234 <139645270+saifimd1234@users.noreply.github.com>
2025-03-14 19:27:15 +0530

    Insert values in silver.erp_loc_a101

--- scripts/Silver/proc_load_silver.sql
+++ scripts/Silver/proc_load_silver.sql
@@ -121,3 +121,17 @@ SELECT
 		ELSE 'n/a'
 END AS gen -- Normalize gender values and handle unknown cases
 FROM bronze.erp_cust_az12;
+
+INSERT INTO silver.erp_loc_a101 (
+        cid,
+        cntry
+    )
+SELECT
+    REPLACE(cid, '-', '_') AS cid,
+    CASE
+        WHEN TRIM(cntry) = 'DE' THEN 'Germany'
+        WHEN TRIM(cntry) IN ('US', 'USA') THEN 'United States'
+        WHEN TRIM(cntry) = '' OR cntry IS NULL THEN 'n/a'
+        ELSE cntry
+    END AS cntry
+FROM bronze.erp_loc_a101;
