66687868cfce8d154f3c703ec8a9d7efece8b26e
saifimd1234 <139645270+saifimd1234@users.noreply.github.com>
2025-03-15 15:51:56 +0530

    Insert into silver.erp_px_cat_g1v2

--- scripts/Silver/proc_load_silver.sql
+++ scripts/Silver/proc_load_silver.sql
@@ -160,3 +160,19 @@ SELECT
     END AS cntry
 FROM bronze.erp_loc_a101;
 
+-- Loading erp_px_cat_g1v2
+PRINT '>> Truncating Table: silver.erp_px_cat_g1v2';
+TRUNCATE TABLE silver.erp_px_cat_g1v2;
+PRINT '>> Inserting Data Into: silver.erp_px_cat_g1v2';
+INSERT INTO silver.erp_px_cat_g1v2 (
+	id,
+	cat,
+	subcat,
+	maintenance
+)
+SELECT
+	id,
+	cat,
+	subcat,
+	maintenance
+FROM bronze.erp_px_cat_g1v2;
