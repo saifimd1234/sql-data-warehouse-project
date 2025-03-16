2806b1c4247c96f92475aabd58469cf9e30de942
saifimd1234 <139645270+saifimd1234@users.noreply.github.com>
2025-03-16 13:54:17 +0530

    Syntax error in ddl_silver.sql

--- scripts/Silver/ddl_silver.sql
+++ scripts/Silver/ddl_silver.sql
@@ -78,7 +78,7 @@ GO
 CREATE TABLE silver.erp_cust_az12 (
     cid    NVARCHAR(50),
     bdate  DATE,
-    gen    NVARCHAR(50)
+    gen    NVARCHAR(50),
 	dwh_create_date DATETIME2 DEFAULT GETDATE()
 );
 GO
