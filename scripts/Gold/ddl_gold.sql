86ea3ab6ec9917084d8f94e35580e4b02ba9cba3
saifimd1234 <139645270+saifimd1234@users.noreply.github.com>
2025-03-16 14:19:27 +0530

    Rename columns and re-arrange it.
    Rename to friendly column names and in logical order.

--- scripts/Gold/ddl_gold.sql
+++ scripts/Gold/ddl_gold.sql
@@ -1,17 +1,19 @@
 SELECT 
     ROW_NUMBER() OVER(ORDER BY cst_id) AS customer_key, -- Surrogate key
-	ci.cst_id,
-    ci.cst_key,
-    ci.cst_firstname,
-    ci.cst_lastname,
-    ci.cst_marital_status,
-    ci.cst_create_date,
+	ci.cst_id                          AS customer_id,
+    ci.cst_key                         AS customer_number,
+    ci.cst_firstname                   AS first_name,
+    ci.cst_lastname                    AS last_name,
+    la.cntry                           AS country,
+    ci.cst_marital_status              AS marital_status,
+    
     CASE
 		WHEN ci.cst_gndr != 'n/a' THEN ci.cst_gndr   -- CRM is the primary source for gender
 		ELSE COALESCE(ca.gen, 'n/a')   -- Fallback to ERP data
-	END gender,
-    ca.bdate,
-    la.cntry
+	END                                AS gender,
+    ca.bdate                           AS birthdate,
+    ci.cst_create_date                 AS create_date
+    
 FROM 
     silver.crm_cust_info AS ci
 LEFT JOIN silver.erp_cust_az12 AS ca 
